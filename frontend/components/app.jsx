113990622946ca597e1d8ae5f1acb10d00c7d335
sijos <ssny88@gmail.com>
2017-03-14 16:36:31 -0700

    setup root and main app components

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = ({ children }) => (
+  <div>
+    <h1>Mai Kewl Site</h1>
+    { children }
+  </div>
+);
+
+export default App;
--- frontend/goDeeper.jsx
+++ frontend/goDeeper.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Root from './components/root';
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  window.store = configureStore();
-  ReactDOM.render(<h1>Site Is Up2</h1>, root);
+  const store = configureStore();
+  ReactDOM.render(<Root store={store} />, root);
 });
