7b7636db53c04342804748471f4f3ed62fb89112
sijos <ssny88@gmail.com>
2017-03-14 14:28:49 -0700

    add application controller auth methods and revise sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,18 +1,23 @@
-class SessionsController < ApplicationController
+class Api::SessionsController < ApplicationController
   def create
     @user = User.find_by_credentials(
       params[:user][:username],
       params[:user][:password])
     if @user
       login(@user)
-      render "api/index"
+      render "api/users/show"
     else
       render json: ["Invalid Username or Password"], status: 401
     end
   end
 
   def destroy
-    logout
-    render "/"
+    @user = current_user
+    if @user
+      logout
+      render "api/users/show"
+    else
+      render json: ["No user logged in"], status: 404
+    end
   end
 end
