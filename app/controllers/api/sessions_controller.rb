5559b7ab0da4a5ba6386804c27361c47a67d929a
sijos <ssny88@gmail.com>
2017-03-14 12:01:33 -0700

    setup sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if @user
+      login(@user)
+      render "api/index"
+    else
+      render json: ["Invalid Username or Password"], status: 401
+    end
+  end
+
+  def destroy
+    logout
+    render "/"
+  end
 end
