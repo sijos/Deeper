bce1dda9ad87b2d6f0c7f8591d3638bb574932d9
sijos <ssny88@gmail.com>
2017-03-14 14:06:16 -0700

    add static pages controller, view and route to root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: {format: :json} do
+  namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :update]
     resource :session, only: [:create, :destroy]
   end
+
+  root "static_pages#root"
 end
--- frontend/goDeeper.jsx
+++ frontend/goDeeper.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './components/root';
-import configureStore from './store/store';
+// import Root from './components/root';
+// import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  const store = configureStore();
-  ReactDOM.render(<h1>Site Up</h1>, root);
+  // const store = configureStore();
+  ReactDOM.render(<h1>Site Is Up</h1>, root);
 });
