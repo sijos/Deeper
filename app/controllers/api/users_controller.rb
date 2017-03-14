8b9fcd3c8e7d28dd7750e2415b768e7897c2c2e7
sijos <ssny88@gmail.com>
2017-03-14 11:56:08 -0700

    add update to users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,7 +4,16 @@ class Api::UsersController < ApplicationController
 
     if @user.save
       login(@user)
-      render "api/index"
+      render "api/index" #for now, update to show profile page once created
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      render "api/index" #for now, update to show profile page once created
     else
       render json: @user.errors.full_messages, status: 422
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :update]
     resource :session, only: [:create, :destroy]
   end
