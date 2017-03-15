29381611b25bfc150fece097d97dc407fe6d2144
sijos <ssny88@gmail.com>
2017-03-15 14:17:39 -0700

    prepare for initial Heroku push

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,12 @@
-import React from 'React';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app';
+// import Home from  './home/home';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
-    <Router history={hashHistory}>
+    <Router history={ hashHistory }>
       <Route path="/" component={ App } />
     </Router>
   </Provider>
