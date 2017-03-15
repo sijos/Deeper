9529c6b76921aaf77f37e9fc11b10141b5ed9228
sijos <ssny88@gmail.com>
2017-03-14 22:22:20 -0700

    enable user bootstrapping for cleaner relaods

--- frontend/goDeeper.jsx
+++ frontend/goDeeper.jsx
@@ -5,10 +5,17 @@ import configureStore from './store/store';
 import { login } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: {currentUser: window.currentUser, errors: []} };
+    //TODO remove errors from preloaded state after forms are setup
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
-  const store = configureStore();
-  window.store = store;
+  window.store = store; //for testing, remove later
   ReactDOM.render(<Root store={store} />, root);
 });
 
-window.login = login;
+window.login = login; //for testing, remove later
