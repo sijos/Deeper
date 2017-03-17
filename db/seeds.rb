b7bb71e14b828cea9d842ec041f5a8aba96bcaf1
sijos <ssny88@gmail.com>
2017-03-17 00:54:46 -0700

    prep seed file for db reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,70 +6,77 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Resort.create({
-#   name: 'Vail',
-#   address: '250 Vail Rd',
-#   zip: '81657',
-#   city: 'Vail',
-#   state_province: 'CO',
-#   country: 'USA',
-#   skiable_acres: 5289,
-#   vertical_drop: 3450,
-#   peak_elevation: 11570,
-#   avg_snowfall: 29.0,
-#   num_trails: 193,
-#   num_lifts: 31,
-#   price_rating: 4,
-#   website_url:'http://www.vail.com/',
-#   trail_map_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708199/resort%20pics/vail-map.pdf',
-#   lat: 39.606138,
-#   lng: -106.354973,
-#   profile_pic_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708055/resort%20pics/vail-profile.jpg'
-#   })
-#
-# Resort.create({
-#   name: 'Heavenly',
-#   address: '4080 Lake Tahoe Blvd',
-#   zip: '96150',
-#   city: 'South Lake Tahoe',
-#   state_province: 'CA',
-#   country: 'USA',
-#   skiable_acres: 4800,
-#   vertical_drop: 3502,
-#   peak_elevation: 10067,
-#   avg_snowfall: 30.0,
-#   num_trails: 97,
-#   num_lifts: 28,
-#   price_rating: 4,
-#   website_url: 'http://www.skiheavenly.com/',
-#   trail_map_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708288/resort%20pics/heavenly-map.jpg',
-#   lat: 38.956829,
-#   lng: -119.942632,
-#   profile_pic_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708465/resort%20pics/heavenly-profile.jpg'
-# })
+User.create([
+  { username: 'scott', password: 'password' },
+  { username: 'zestyyy', password: 'password' },
+  { username: 'rage', password: 'password' },
+  { username: 'ridelyfe', password: 'password' },
+])
 
-Resort.create({
-  name: 'Whistler Blackcomb',
-  address: '4545 Blackcom Way',
-  zip: 'V0N 1B4',
-  city: 'Whistler',
-  state_province: 'BC',
-  country: 'Canada',
-  skiable_acres: 8171,
-  vertical_drop: 5279,
-  peak_elevation: 7493,
-  avg_snowfall: 38.5,
-  num_trails: 200,
-  num_lifts: 37,
-  price_rating: 3,
-  website_url: 'https://www.whistlerblackcomb.com/',
-  trail_map_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708572/resort%20pics/whistler-map.pdf',
-  lat: 50.114964,
-  lng: -122.948647,
-  profile_pic_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708609/resort%20pics/whistler-profile.jpg',
-})
+Resort.create([
+  {
+    name: 'Vail',
+    address: '250 Vail Rd',
+    zip: '81657',
+    city: 'Vail',
+    state_province: 'CO',
+    country: 'USA',
+    skiable_acres: 5289,
+    vertical_drop: 3450,
+    peak_elevation: 11570,
+    avg_snowfall: 29.0,
+    num_trails: 193,
+    num_lifts: 31,
+    price_rating: 4,
+    website_url:'http://www.vail.com/',
+    trail_map_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708199/resort%20pics/vail-map.pdf',
+    lat: 39.606138,
+    lng: -106.354973,
+    profile_pic_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708055/resort%20pics/vail-profile.jpg'
+  },
+  {
+    name: 'Heavenly',
+    address: '4080 Lake Tahoe Blvd',
+    zip: '96150',
+    city: 'South Lake Tahoe',
+    state_province: 'CA',
+    country: 'USA',
+    skiable_acres: 4800,
+    vertical_drop: 3502,
+    peak_elevation: 10067,
+    avg_snowfall: 30.0,
+    num_trails: 97,
+    num_lifts: 28,
+    price_rating: 4,
+    website_url: 'http://www.skiheavenly.com/',
+    trail_map_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708288/resort%20pics/heavenly-map.jpg',
+    lat: 38.956829,
+    lng: -119.942632,
+    profile_pic_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708465/resort%20pics/heavenly-profile.jpg'
+  },
+  {
+    name: 'Whistler Blackcomb',
+    address: '4545 Blackcom Way',
+    zip: 'V0N 1B4',
+    city: 'Whistler',
+    state_province: 'BC',
+    country: 'Canada',
+    skiable_acres: 8171,
+    vertical_drop: 5279,
+    peak_elevation: 7493,
+    avg_snowfall: 38.5,
+    num_trails: 200,
+    num_lifts: 37,
+    price_rating: 3,
+    website_url: 'https://www.whistlerblackcomb.com/',
+    trail_map_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708572/resort%20pics/whistler-map.pdf',
+    lat: 50.114964,
+    lng: -122.948647,
+    profile_pic_url: 'http://res.cloudinary.com/dagjelvab/image/upload/v1489708609/resort%20pics/whistler-profile.jpg',
+  }
+])
 
-# Resort.create({
+# {
 #   name:
 #   address:
 #   zip:
@@ -88,4 +95,4 @@ Resort.create({
 #   lat:
 #   lng:
 #   profile_pic_url:
-# })
+# }
