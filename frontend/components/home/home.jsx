c1326ad4b6e4f245e946c6425c51fa574affc4c3
sijos <ssny88@gmail.com>
2017-03-16 23:26:22 -0700

    complete initial styling on main page mtn index

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,13 +3,15 @@ import ResortIndexContainer from './resort_index_container';
 
 const Home = () => (
   <div>
+    <div>
       <video loop muted autoPlay className="fullscreen-bg__video">
         <source src="http://res.cloudinary.com/dagjelvab/video/upload/v1489725952/main_page_video_i5mhk1.mp4" type="video/mp4"/>
       </video>
-    <nav className="landing">
-      <h1 className="title">Steeper</h1>
-      <h4 className="tagline">Your guide to the world's greatest mountains</h4>
-    </nav>
+      <nav className="landing">
+        <h1 className="title">Steeper</h1>
+        <h4 className="tagline">Your guide to the world's greatest mountains</h4>
+      </nav>
+    </div>
     <ResortIndexContainer />
   </div>
 );
--- frontend/components/home/resort_index.jsx
+++ frontend/components/home/resort_index.jsx
@@ -11,11 +11,19 @@ class ResortIndex extends React.Component {
 
   render() {
     return (
-      <ul className="resort-index">
-        {this.props.resorts.map(resort => (
-          <li key={resort.id}>{resort.name}</li>
-        ))}
-      </ul>
+      <div className="index-container">
+        <h2>Popular Mountains</h2>
+        <ul className="resort-index">
+          {this.props.resorts.map(resort => (
+            <li key={resort.id}>
+              <div className="resort-item">
+                <img className="resort-photo" src={resort.profile_pic_url} />
+                {resort.name}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
