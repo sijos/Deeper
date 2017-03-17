7928f7097f6accd852e6d31f384be5a900234507
sijos <ssny88@gmail.com>
2017-03-17 15:12:43 -0700

    update seed file

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create, :update]
     resource :session, only: [:create, :destroy]
     resources :resorts, only: [:index, :show]
+    resources :reviews, only: [:update, :destroy]
   end
 
   root "static_pages#root"
