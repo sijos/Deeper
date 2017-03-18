103e82b93c5084d1c1acbcf0dbc2de2f47acb86a
sijos <ssny88@gmail.com>
2017-03-17 20:15:05 -0700

    setup review actions

---
+++ frontend/actions/review_actions.js
@@ -0,0 +1,41 @@
+import * as ReviewAPI from '../util/reviews_api_util';
+
+export const RECEIVE_REVIEW = "RECEIVE_REVIEW";
+export const REMOVE_REVIEW = "REMOVE_REVIEW";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+const receiveReview = (review) => ({
+  type: RECEIVE_REVIEW,
+  review
+});
+
+const removeReview = (review) => ({
+  type: REMOVE_REVIEW,
+  review
+});
+
+const receiveErrors = (errors) => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const postReview = (review) => (dispatch) => {
+  ReviewAPI.postReview(review).then(
+    (review) => dispatch(receiveReview(review)),
+    (error) => dispatch(receiveErrors(error.responseJSON))
+  );
+};
+
+export const updateReview = (review) => (dispatch) => {
+  ReviewAPI.updateReview(review).then(
+    (review) => dispatch(receiveReview(review)),
+    (error) => dispatch(receiveErrors(error.responseJSON))
+  );
+};
+
+export const deleteReview = (review) => (dispatch) => {
+  ReviewAPI.deleteReview(review).then(
+    (review) => dispatch(removeReview(review)),
+    (error) => dispatch(receiveErrors(error.responseJSON))
+  );
+};
