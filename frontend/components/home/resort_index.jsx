8c93bc8d7868290c9c49f02bae1adce92047908c
sijos <ssny88@gmail.com>
2017-03-18 01:29:07 -0700

    correct styling issue on resort detail

--- frontend/components/home/resort_index.jsx
+++ frontend/components/home/resort_index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { hashHistory } from 'react-router';
 
 class ResortIndex extends React.Component {
   constructor(props) {
@@ -9,13 +10,21 @@ class ResortIndex extends React.Component {
     this.props.fetchResorts();
   }
 
+  routeToResort(id) {
+    return (e) => {
+      e.preventDefault();
+      hashHistory.push(`/resorts/${id}`);
+    };
+  }
+
   render() {
     return (
       <div className="index-container">
         <h2>Popular Mountains</h2>
         <ul className="resort-index">
           {this.props.resorts.map(resort => (
-            <li key={resort.id} className="resort-item">
+            <li key={resort.id} className="resort-item"
+              onClick={this.routeToResort(resort.id)}>
               <img className="resort-photo" src={resort.profile_pic_url} />
               {resort.name}
             </li>
