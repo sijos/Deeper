e91c9c606bedcc227fcdd7fb26d51d4bb8a4f41c
sijos <ssny88@gmail.com>
2017-03-19 12:26:56 -0700

    enable clickalbe logo that routes to index

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -1,30 +1,39 @@
 import React from 'react';
 import AuthContainer from '../auth/auth_container';
 import SearchBar from '../search_bar/search_bar';
+import { hashHistory } from 'react-router';
 
-const NavBar = ({ navType }) => {
-  let navClass, headerClass, searchBar, logo;
-  if (navType === "search") {
-    navClass = "search-nav-bar";
-    headerClass = "search-header";
-    searchBar = <SearchBar />;
-    logo = <img className="logo" src='assets/Logo-white.png' />;
-  } else {
-    navClass = "nav-bar";
-    headerClass = "header";
-    logo = <img className="logo" src='assets/Logo.png' />;
+class NavBar extends React.Component {
+
+  routeHome() {
+    hashHistory.push('/');
   }
 
+  render () {
+    let navClass, headerClass, searchBar, logo;
+    if (this.props.navType === "search") {
+      navClass = "search-nav-bar";
+      headerClass = "search-header";
+      searchBar = <SearchBar />;
+      logo = <img className="logo" src='assets/Logo-white.png' />;
+    } else {
+      navClass = "nav-bar";
+      headerClass = "header";
+      logo = <img className="logo" src='assets/Logo.png' />;
+    }
 
-  return (
-    <header className={headerClass}>
-      <nav className={navClass}>
-        {logo}
-        {searchBar}
-        <AuthContainer navClass={navClass}/>
-      </nav>
-    </header>
-  );
-};
+    return (
+      <header className={headerClass}>
+        <nav className={navClass}>
+          <div className="clickable" onClick={this.routeHome}>
+            {logo}
+          </div>
+          {searchBar}
+          <AuthContainer navClass={navClass}/>
+        </nav>
+      </header>
+    );
+  }
+}
 
 export default NavBar;
