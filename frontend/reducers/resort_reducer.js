8124b89677c3f57b65bd5369260ba02cf0cc6e23
sijos <ssny88@gmail.com>
2017-03-22 20:03:44 -0700

    remove extra reducer and jbuilder functionality for resort show

--- frontend/reducers/resort_reducer.js
+++ frontend/reducers/resort_reducer.js
@@ -9,26 +9,12 @@ const _defaultResort = {
   errors: []
 };
 
-const ResortReducer = (state = _defaultResort, action) => {
+const ResortReducer = (state = {}, action) => {
   Object.freeze(state);
   let newState;
   switch (action.type) {
     case RECEIVE_RESORT:
-      return merge({}, _defaultResort, action.resort);
-    case RECEIVE_REVIEW:
-      newState = merge({}, _defaultResort, state);
-      newState.reviews.unshift(action.review);
-      newState.num_reviews += 1;
-      let newRatings = newState.reviews.map(review => review.overall_rating);
-      newState.avg_rating =
-        newRatings.reduce((a, b) => a + b, 0) / newState.num_reviews;
-      return newState;
-    case RECEIVE_REVIEW_ERRORS:
-      let errors = action.errors;
-      return merge({}, state, {errors});
-    case CLEAR_REVEIW_ERRORS:
-      newState = merge({}, state);
-      return Object.assign(newState, {errors: []});
+      return merge({}, action.resort);
     default:
       return state;
   }
